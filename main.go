4658901d37493cd1be88b49a7600b27e1de5afc3
Damas <dam6660@rit.edu>
2016-08-13 13:09:47 -0700

    Added procfile for heroku

--- main.go
+++ main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"os"
+	"strconv"
+
 	"github.com/astaxie/beego"
 	_ "github.com/damagination/makler/routers"
 )
 
 func main() {
+	port, err := strconv.Atoi(os.Getenv("PORT"))
+	if err == nil {
+		beego.HttpPort = port
+	}
 	beego.Run()
 }
