6ca2897bfaf2c6444986a0c35a02336a7e2fa878
Damas <dam6660@rit.edu>
2016-08-13 12:26:27 -0700

    remove debug mode

--- main.go
+++ main.go
@@ -2,11 +2,9 @@ package main
 
 import (
 	"github.com/astaxie/beego"
-	"github.com/astaxie/beego/orm"
 	_ "github.com/damagination/makler/routers"
 )
 
 func main() {
-	orm.Debug = true
 	beego.Run()
 }
