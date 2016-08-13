ef2c486b3760e47912fd2bed798925ba0606e580
Damas <dam6660@rit.edu>
2016-08-13 13:21:15 -0700

    HttpPort undefined workaroud

--- main.go
+++ main.go
@@ -2,16 +2,13 @@ package main
 
 import (
 	"os"
-	"strconv"
 
 	"github.com/astaxie/beego"
 	_ "github.com/damagination/makler/routers"
 )
 
 func main() {
-	port, err := strconv.Atoi(os.Getenv("PORT"))
-	if err == nil {
-		beego.HttpPort = port
-	}
-	beego.Run()
+	port := os.Getenv("PORT")
+	port = ":" + port
+	beego.Run(port)
 }
