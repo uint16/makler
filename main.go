5a735e630495e4bad55a823d59033ff27c240e36
Damas <dam6660@rit.edu>
2016-08-15 23:44:40 -0700

    ignore db creation, causing errors on heroku

--- main.go
+++ main.go
@@ -4,16 +4,15 @@ import (
 	"os"
 
 	"github.com/astaxie/beego"
-	"github.com/astaxie/beego/orm"
 	_ "github.com/damagination/makler/routers"
 )
 
 func main() {
 
-	err := orm.RunSyncdb("db2", false, false)
-	if err != nil {
-
-	}
+	// err := orm.RunSyncdb("db2", false, false)
+	// if err != nil {
+	//
+	// }
 	port := os.Getenv("PORT")
 	if len(port) == 0 {
 		port = "8080"
